eb782732e9dd5c8753fa2dfb0e10ab46eab14243
Sergio Schmilovich <sergio.schmilovich@gmail.com>
2022-06-17 16:45:53 -0500

    Added coment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from utils import make_dir
 from publish import publish_to_mqtt
 
+## Needs https://github.com/andyvorld/LGSTrayBattery installed and running
+
 def main():
     make_dir('data')
     publish_to_mqtt()
